7766dec4d1c9fbccef01c491c9ba457df62d6247
Jiri Stransky <jistr@redhat.com>
2020-01-07 14:14:25 +0000

    Add network serialization stub code
    The data from serialized network is split between "params" and "info",
    the idea being that params are important values which are needed when
    importing the network, while info are not used when importing but
    might be useful for a human looking through the exported data.

--- os_migrate/plugins/module_utils/serialization.py
+++ os_migrate/plugins/module_utils/serialization.py
@@ -1,2 +1,43 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
+
+
+def serialize_network(network):
+    resource = {}
+    params = {}
+    info = {}
+    resource['params'] = params
+    resource['info'] = info
+    resource['type'] = 'openstack.network'
+
+    params['availability_zone_hints'] = network['availability_zone_hints']
+    params['availability_zones'] = network['availability_zones']
+    params['description'] = network['description']
+    params['dns_domain'] = network['dns_domain']
+    params['is_admin_state_up'] = network['is_admin_state_up']
+    params['is_default'] = network['is_default']
+    params['is_port_security_enabled'] = network['is_port_security_enabled']
+    params['is_router_external'] = network['is_router_external']
+    params['is_shared'] = network['is_shared']
+    params['is_vlan_transparent'] = network['is_vlan_transparent']
+    params['mtu'] = network['mtu']
+    params['name'] = network['name']
+    params['provider_network_type'] = network['provider_network_type']
+    params['provider_physical_network'] = network['provider_physical_network']
+    params['provider_segmentation_id'] = network['provider_segmentation_id']
+    params['qos_policy_id'] = network['qos_policy_id']
+    params['revision_number'] = network['revision_number']
+    params['segments'] = network['segments']
+
+    info['created_at'] = network['created_at']
+    info['project_id'] = network['project_id']
+    info['status'] = network['status']
+    info['subnet_ids'] = network['subnet_ids']
+    info['updated_at'] = network['updated_at']
+
+    # TODO: Add a (cached?) lookup for names of id-like properties.
+    #     params['qos_policy_name']
+    #     info['project_name']
+    #     info['subnet_names']
+
+    return resource
---
+++ os_migrate/tests/unit/fixtures.py
@@ -0,0 +1,34 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import openstack
+
+
+def network():
+    return openstack.network.v2.network.Network(
+        availability_zone_hints=['nova', 'zone2'],
+        availability_zones=['nova', 'zone3'],
+        created_at='2020-01-06T15:50:55Z',
+        description='test network',
+        dns_domain='example.org',
+        ipv4_address_scope_id=None,
+        ipv6_address_scope_id=None,
+        is_admin_state_up=True,
+        is_default=False,
+        is_port_security_enabled=True,
+        is_router_external=False,
+        is_shared=False,
+        mtu=1400,
+        name='test-net',
+        project_id='uuid-test-project',
+        provider_network_type='vxlan',
+        provider_physical_network='physnet',
+        provider_segmentation_id='456',
+        qos_policy_id='uuid-test-qos-policy',
+        revision_number=3,
+        segments=[],
+        status='ACTIVE',
+        subnet_ids=['uuid-test-subnet1', 'uuid-test-subnet2'],
+        updated_at='2020-01-06T15:51:00Z',
+        is_vlan_transparent=False,
+    )
