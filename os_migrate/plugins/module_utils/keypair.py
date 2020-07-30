df13a40cae86b23099c30d2de3af50ba3c2cbcb9
Ryan Brady <rbrady@redhat.com>
2020-07-30 09:52:23 -0400

    Adds Roles For Nova Keypair Import and Export
    This patch adds the roles and associated code required to export
    and import Nova keypairs.

---
+++ os_migrate/plugins/module_utils/keypair.py
@@ -0,0 +1,42 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import openstack
+
+from ansible_collections.os_migrate.os_migrate.plugins.module_utils \
+    import const, resource
+
+
+class Keypair(resource.Resource):
+    # according to https://github.com/openstack/openstacksdk/blob/a4a2a7b42ec2ae7e186b44aeb7242fddd84944f7/openstack/cloud/_compute.py#L601
+    # keypairs are created with name and public key.  user is not used.
+    resource_type = const.RES_TYPE_KEYPAIR
+    sdk_class = openstack.compute.v2.keypair.Keypair
+
+    info_from_sdk = [
+        'created_at',
+        'id',
+        'user_id',
+        'fingerprint',
+        'type',
+        'is_deleted',
+    ]
+
+    params_from_sdk = [
+        'name',
+        'private_key',
+        'public_key',
+    ]
+
+    @staticmethod
+    def _create_sdk_res(conn, sdk_params):
+        return conn.compute.create_keypair(**sdk_params)
+
+    @staticmethod
+    def _find_sdk_res(conn, name_or_id, filters=None):
+        return conn.compute.find_keypair(name_or_id, **(filters or {}))
+
+    @staticmethod
+    def _update_sdk_res(conn, sdk_res, sdk_params):
+        # openstack.compute.v2.keypair.Keypair does not support update
+        return
