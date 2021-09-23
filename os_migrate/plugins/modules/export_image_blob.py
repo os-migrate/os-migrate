258cc7b6a12c72b5be98bc908a084f180a0ad9bc
Philip Roche <phil@philroche.net>
2021-09-23 18:41:31 +0100

    Dev: Prepare os-migrate for ansible version bump in toolbox build to latest known working version 4.5.0
    ansible 4.5.0 is known to work for all e2e and functional tests so we can now
    prepare for the ansible version installed in the toolbox virtualenv to 4.5.0. Below are
    description of the changes required to allow this version bump.
    
    Version bump will not be made until we have CI in place to allow testing of multiple ansible versions.
    
    The default version will remain `ansible>=2.9.1,<2.10`
    
    ------------------------------------------------------------------------------------
    Fix: ansible package from package manager is only required when vagrant usage in dev toolbox is expected
    
    Ansible package from package manager is not required for non vagrant usage as
    ansible is installed from PyPi in a python virtual environment.
    
    The only reason it is required for vagrant is during vagrant provisioning as
    we use the ansible provisioner which requires that the ansible and ansible-playbook
    executables be in $PATH.
    ------------------------------------------------------------------------------------
    Fix: Add ability to uninstall requirements in venv before override
    
    When using OS_MIGRATE_REQUIREMENTS_OVERRIDE environment variable to
    downgrade requirements it might also be required to uninstall too.
    This is the case when downgrading from ansible>=3 to ansible<3 due
    to ansible>=3 installing ansible-base or ansible-core.
    
    User as following:
    
    ```
    export OS_MIGRATE_REQUIREMENTS_UNINSTALL_BEFORE_OVERRIDE='ansible ansible-base ansible-core'
    
    ```
    ------------------------------------------------------------------------------------
    Fix: Pin lower bound python-openstackclient requirement
    
    python-openstackclient>5.3.0 requires openstacksdk>=0.48.0, but we pin
    openstacksdk to openstacksdk 0.36.5 which is incompatible with
    python-openstackclient>5.3.0. We avoid this incompatibility by
    pinning python-openstackclient<5.3.0.
    ------------------------------------------------------------------------------------
    Fix: Import openstack module utils from ansible_collections.openstack.cloud.plugins as per ansible 3+
    
    try/except import to add support for ansible 3+ while maintaining support for ansible 2.9
    Fix: Add new ansible version bump to the os_migrate ansible collection runtime requirements
    
    os_migrate now support up to and including ansible 4.5

--- os_migrate/plugins/modules/export_image_blob.py
+++ os_migrate/plugins/modules/export_image_blob.py
@@ -76,8 +76,14 @@ RETURN = '''
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.openstack \
-    import openstack_full_argument_spec, openstack_cloud_from_module
+# Import openstack module utils from ansible_collections.openstack.cloud.plugins as per ansible 3+
+try:
+    from ansible_collections.openstack.cloud.plugins.module_utils.openstack \
+        import openstack_full_argument_spec, openstack_cloud_from_module
+except ImportError:
+    # If this fails fall back to ansible < 3 imports
+    from ansible.module_utils.openstack \
+        import openstack_full_argument_spec, openstack_cloud_from_module
 
 from ansible_collections.os_migrate.os_migrate.plugins.module_utils import image
 
