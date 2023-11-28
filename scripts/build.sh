f6fe82cfa019219608ec053a5186f4fa96725421
cloudguruab <adrbrownx@gmail.com>
2023-11-28 16:54:09 +0100

    dev: test

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,9 +15,11 @@ fi
 
 # Apply virtualenv version overrides if defined
 if [ -n "${OS_MIGRATE_REQUIREMENTS_OVERRIDE:-}" ]; then
+    python3 -m pip cache purge && rm -rf $(python3 -m pip cache dir)
     python3 -m pip cache purge && rm -rf $cache_dir
     sleep 1
     python3 -m pip install --upgrade pip
+    sleep 1
     python3 -m pip cache purge && rm -rf $cache_dir
 
     # Workaround for ERROR: Could not install packages due to an
