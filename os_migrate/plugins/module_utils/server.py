c8eda4196fc48f90b7636affa9dba7a8c6b565e4
cloudguruab <ap.brown011@gmail.com>
2024-11-05 23:57:14 +0100

    dev: ensure we checks for conv host activity for each migration mode

--- os_migrate/plugins/module_utils/server.py
+++ os_migrate/plugins/module_utils/server.py
@@ -104,12 +104,10 @@ class Server(resource.Resource):
         # support for advanced port creation via Neutron.
         self.update_sdk_params_networks_simple(conn, sdk_params, port_creation_mode)
 
-        # KeyError here with recent changes.
         if not self.migration_params()['data_copy']:
             self.update_sdk_params_block_device_mapping_nocopy(sdk_params)
         else:
-            # self.update_sdk_params_block_device_mapping_copy(sdk_params, block_device_mapping)
-            return {"sdk_params": sdk_params, "block_device_mapping": block_device_mapping}
+            self.update_sdk_params_block_device_mapping_copy(sdk_params, block_device_mapping)
 
         sdk_srv = conn.compute.create_server(**sdk_params)
         # Wait for the server before we attach Floating IPs, otherwise
